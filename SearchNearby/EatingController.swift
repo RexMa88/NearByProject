c3c6eecc699c2e4442e09bbc729f74dfa98f5d7e
RexMa88 <rex.smith98@gmail.com>
2016-02-05 14:19:30 +0800

    add Project

---
+++ SearchNearby/EatingController.swift
@@ -0,0 +1,35 @@
+//
+//  EatingController.swift
+//  SearchNearby
+//
+//  Created by 马超 on 16/2/4.
+//  Copyright © 2016年 RexMa. All rights reserved.
+//
+
+import UIKit
+
+class EatingController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
